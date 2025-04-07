a762d6882ac6a12a856259e691e3082654cc3a38
LaisZagati <laisviana97@gmail.com>
2025-04-07 19:05:25 +0100

    Calculate surplus data

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 # Import necessary libraries
 import gspread  # Library for interacting with Google Sheets
 from google.oauth2.service_account import Credentials  # Handles authentication
+from pprint import pprint
+
 
 # Define the required scopes (permissions) for Google Sheets and Google Drive access
 SCOPE = [
@@ -96,7 +98,14 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+
+    
+    surplus_data =[]
+    for stock, sales in zip (stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
         
     
     
@@ -108,7 +117,8 @@ def main():
 data = get_sales_data()
 sales_data = [int(num) for num in data]
 update_sales_worksheet(sales_data)
-calculate_surplus_data(sales_data)
+new_surplus_data = calculate_surplus_data(sales_data)
+print(new_surplus_data)
       
 
 print("Welcome to Love Sandwiches Data Automation")
